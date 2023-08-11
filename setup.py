d7b6380fea70036a36962bd6393dc616ff4871d3
Dipak Tripathi <dipaktripathi10.com>
2023-08-11 16:07:20 +0530

    Added setup.y and updated requirements.txt file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+from typing import List
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
+
+def get_requirement()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_file.replace("\n","") for requirement_name in requirement_list]
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+setup(
+    name="sensor",
+    version = "0.0.1",
+    author = "ineuron",
+    author_email = "dipaktripathi10@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+)
