37ee0cb201e35674aa82b2ee6f1c35c907023d84
Dipak Tripathi <dipaktripathi10@gmail.com>
2023-09-02 18:43:37 +0530

    Added complete code for Model train and Predict

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
+from sensor.components.model_evaluation import ModelEvaluation
+from sensor.components.model_pusher import ModelPusher
 
 
 if __name__=="__main__":
@@ -30,12 +32,28 @@ if __name__=="__main__":
           data_transformation = DataTransformation(data_transformation_config=data_transformation_config,
           data_ingestion_artifact=data_ingestion_artifact)
 
-          data_validation_artifact = data_transformation.initiate_data_transormation()
+          data_transformation_artifact = data_transformation.initiate_data_transormation()
 
           #model Trainer
           model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
           model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_config)
           model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+          #Model Evaluation
+          model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+          model_eval = ModelEvaluation(model_eval_config=model_eval_config, 
+                                   data_ingestion_artifact=data_ingestion_artifact, 
+                                   data_transformation_artifact=data_transformation_artifact, 
+                                   model_trainer_artifact=model_trainer_artifact)
+          model_evel_artifact = model_eval.initiate_model_evaluation()
+
+          #Model Pusher
+          model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config=training_pipeline_config)
+          
+          model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
+                                   data_transformation_artifact=data_transformation_artifact,
+                                   model_trainer_artifact=model_trainer_artifact)
+          model_pusher_artifact = model_pusher.initiate_model_pusher()
 
      
      except Exception as e:
--- sensor/predictor.py
+++ sensor/predictor.py
@@ -1,5 +1,5 @@
 import os
-from sensor.entity.config_entity import TRANSFORMER_OBJECT_FILE_NAME,MODEL_FILE_NAME,TARGET_ENCODER_OBJECT_FILE_NAME
+from sensor.entity.config_entity import TRANSFORMER_OBJECT_FILENAME, MODEL_FILE_NAME, TARGET_ENCODER_OBJECT_FILE_NAME
 from glob import glob
 from typing import Optional
 import os, sys
@@ -24,13 +24,15 @@ class ModelResolver:
                 return None
             dir_names = list(map(int, dir_names))
             latest_folder_name = max(dir_names)
-            return os.path.join(self.model_registry, latest_folder_name)
+            return os.path.join(self.model_registry, f"{latest_folder_name}")
         except Exception as e:
             raise SensorException(e, sys)
 
     def get_latest_model_path(self):
         try:
             latest_dir = self.get_latest_dir_path()
+            if latest_dir is None:
+                raise Exception("Model is not available")
             return os.path.join(latest_dir, self.model_dir_name, MODEL_FILE_NAME)
         except Exception as e:
             raise SensorException(e, sys)
@@ -38,13 +40,17 @@ class ModelResolver:
     def get_latest_transformer_path(self):
         try:
             latest_dir = self.get_latest_dir_path()
-            return os.path.join(latest_dir, self.transformer_model_dir, TRANSFORMER_OBJECT_FILE_NAME)
+            if latest_dir is None:
+                raise Exception("Transformer is not available")
+            return os.path.join(latest_dir, self.transformer_dir_name, TRANSFORMER_OBJECT_FILENAME)
         except Exception as e:
             raise SensorException(e, sys)
 
     def get_latest_target_encoder_path(self):
         try:
             latest_dir = self.get_latest_dir_path()
+            if latest_dir is None:
+                raise Exception("Target Encoder is not available")
             return os.path.join(latest_dir, self.target_encoder_dir_name, TARGET_ENCODER_OBJECT_FILE_NAME)
         except Exception as e:
             raise SensorException(e, sys)
@@ -53,9 +59,9 @@ class ModelResolver:
         try:
             latest_dir = self.get_latest_dir_path()
             if latest_dir==None:
-                os.path.join(self.model_registry, 0)
+                return os.path.join(self.model_registry, f"{0}")
             latest_dir_num = int(os.path.basename(self.get_latest_dir_path()))
-            return os.path.join(self.model_registry, latest_dir_num+1)
+            return os.path.join(self.model_registry, f"{latest_dir_num+1}")
         except Exception as e:
             raise SensorException(e, sys)
 
@@ -69,7 +75,7 @@ class ModelResolver:
     def get_latest_save_transformer_path(self):
         try:
             latest_dir = self.get_latest_save_dir_path()
-            return os.path.join(latest_dir,self.transformer_dir_name,TRANSFORMER_OBJECT_FILE_NAME)
+            return os.path.join(latest_dir,self.transformer_dir_name,TRANSFORMER_OBJECT_FILENAME)
         except Exception as e:
             raise SensorException(e, sys)
 
